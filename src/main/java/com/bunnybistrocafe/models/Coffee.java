3802c56a3c86696be3abd76b75a0f36c6c4724a3
Jasmine <jasminetntu@gmail.com>
2025-11-10 15:24:14 -0800

    implement Coffee.java (#16)

--- src/main/java/com/bunnybistrocafe/models/Coffee.java
+++ src/main/java/com/bunnybistrocafe/models/Coffee.java
@@ -1,4 +1,56 @@
 package com.bunnybistrocafe.models;
 
+import com.bunnybistrocafe.enumerations.drinkcustomizations.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Coffee extends Drink {
+    private MilkType milk;
+    private ArrayList<CoffeeFlavor> flavors; //max 2
+
+    // *** CONSTRUCTORS ***
+    public Coffee(DrinkSize size, DrinkType type, SweetenerType sweetener, double sweetnessLevel,
+                  boolean isIced, double iceLevel, ArrayList<Topping> toppings, boolean hasPlushie,
+                  MilkType milk, ArrayList<CoffeeFlavor> flavors) {
+        super(size, type, sweetener, sweetnessLevel, isIced, iceLevel, toppings, hasPlushie);
+        this.milk = milk;
+        this.flavors = flavors;
+    }
+
+    // *** GETTERS ***
+    public MilkType getMilk() {
+        return milk;
+    }
+
+    public List<CoffeeFlavor> getFlavors() {
+        return Collections.unmodifiableList(flavors);
+    }
+
+    @Override
+    public double getPrice() {
+        double totalPrice = calculateSharedPrice();
+        totalPrice += milk.getPrice();
+
+        if (!flavors.isEmpty()) { //add constant flavor price
+            totalPrice += flavors.size() * flavors.get(0).getPrice();
+        }
+
+        return totalPrice;
+    }
+
+    // *** SETTERS ***
+    public void setMilk(MilkType milk) {
+        this.milk = milk;
+    }
+
+    public void addFlavor(CoffeeFlavor flavor) {
+        flavors.add(flavor);
+    }
+
+    public void removeFlavor(CoffeeFlavor flavor) {
+        flavors.remove(flavor);
+    }
+
 }
