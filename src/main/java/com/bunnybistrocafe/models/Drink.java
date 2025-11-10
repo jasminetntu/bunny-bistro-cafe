5ee6dbb405a84ff1e8bad9122864466554f53691
Jasmine <jasminetntu@gmail.com>
2025-11-10 14:43:30 -0800

    implement Drink class (#14)

--- src/main/java/com/bunnybistrocafe/models/Drink.java
+++ src/main/java/com/bunnybistrocafe/models/Drink.java
@@ -1,4 +1,151 @@
 package com.bunnybistrocafe.models;
 
+import com.bunnybistrocafe.enumerations.drinkcustomizations.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringJoiner;
+
 public abstract class Drink implements MenuItem {
+    private DrinkSize size;
+    private DrinkType type;
+    private SweetenerType sweetener;
+    private double sweetnessLevel;
+    private boolean isIced;
+    private double iceLevel;
+    private ArrayList<Topping> toppings;
+    private boolean hasPlushie;
+
+    // *** CONSTRUCTORS ***
+
+    public Drink(DrinkSize size, DrinkType type, SweetenerType sweetener, double sweetnessLevel,
+                 boolean isIced, double iceLevel, ArrayList<Topping> toppings, boolean hasPlushie) {
+        this.size = size;
+        this.type = type;
+        this.sweetener = sweetener;
+        this.sweetnessLevel = sweetnessLevel;
+        this.isIced = isIced;
+        this.iceLevel = iceLevel;
+        this.toppings = toppings;
+        this.hasPlushie = hasPlushie;
+    }
+
+    // *** GETTERS ***
+
+    public DrinkSize getSize() {
+        return size;
+    }
+
+    public DrinkType getType() {
+        return type;
+    }
+
+    public SweetenerType getSweetener() {
+        return sweetener;
+    }
+
+    public double getSweetnessLevel() {
+        return sweetnessLevel;
+    }
+
+    public boolean isIced() {
+        return isIced;
+    }
+
+    public double getIceLevel() {
+        return iceLevel;
+    }
+
+    public List<Topping> getToppings() {
+        return Collections.unmodifiableList(toppings);
+    }
+
+    public int getNumToppings() {
+        return toppings.size();
+    }
+
+    public boolean hasPlushie() {
+        return hasPlushie;
+    }
+
+    // *** SETTERS ***
+    public void setSize(DrinkSize size) {
+        this.size = size;
+    }
+
+    public void setType(DrinkType type) {
+        this.type = type;
+    }
+
+    public void setSweetener(SweetenerType sweetener) {
+        this.sweetener = sweetener;
+    }
+
+    public void setSweetnessLevel(double sweetnessLevel) {
+        this.sweetnessLevel = sweetnessLevel;
+    }
+
+    public void setIced(boolean iced) {
+        isIced = iced;
+    }
+
+    public void setIceLevel(double iceLevel) {
+        this.iceLevel = iceLevel;
+    }
+
+    public void addTopping(Topping topping) {
+        this.toppings.add(topping);
+    }
+
+    public void removeTopping(Topping topping) {
+        this.toppings.remove(topping);
+    }
+
+    public void setHasPlushie(boolean hasPlushie) {
+        this.hasPlushie = hasPlushie;
+    }
+
+    // *** OTHER ***
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        //size
+        sb.append(size.getName());
+
+        //iced or hot
+        if (isIced) {
+            sb.append(" Iced ");
+        } else {
+            sb.append(" Hot ");
+        }
+
+        //drink type & sweetness
+        sb.append(type.getName())
+                .append("\n").append((int) sweetnessLevel * 100)
+                .append("% ").append(sweetener.getName()).append("\n");
+
+        //ice level
+        if (isIced) {
+            sb.append((int) iceLevel * 100).append("% Ice");
+        }
+
+        //toppings
+        sb.append("\nToppings: ");
+        for (int i = 0; i < toppings.size(); ++i) {
+            sb.append(toppings.get(i).getName());
+            if (i != toppings.size() - 1) {
+                sb.append(", ");
+            }
+        }
+
+        //plushie
+        if (hasPlushie) {
+            sb.append("\nPlushie Cup Upgrade");
+        }
+
+        return sb.toString();
+    }
 }
