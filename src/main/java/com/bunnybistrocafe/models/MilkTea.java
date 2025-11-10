4a9a8869949d4aa7f4c71085a7bee8bb453bdd79
Jasmine <jasminetntu@gmail.com>
2025-11-10 15:24:39 -0800

    implement MilkTea.java (#17)

--- src/main/java/com/bunnybistrocafe/models/MilkTea.java
+++ src/main/java/com/bunnybistrocafe/models/MilkTea.java
@@ -1,4 +1,65 @@
 package com.bunnybistrocafe.models;
 
+import com.bunnybistrocafe.enumerations.drinkcustomizations.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class MilkTea extends Drink {
+    private TeaType teaType;
+    private MilkType milk;
+    private ArrayList<TeaFlavor> flavors; //max 3
+
+    // *** CONSTRUCTORS ***
+    public MilkTea(DrinkSize size, DrinkType type, SweetenerType sweetener, double sweetnessLevel,
+                   boolean isIced, double iceLevel, ArrayList<Topping> toppings, boolean hasPlushie,
+                   TeaType teaType, MilkType milk, ArrayList<TeaFlavor> flavors) {
+        super(size, type, sweetener, sweetnessLevel, isIced, iceLevel, toppings, hasPlushie);
+        this.teaType = teaType;
+        this.milk = milk;
+        this.flavors = flavors;
+    }
+
+    // *** GETTERS ***
+    public TeaType getTeaType() {
+        return teaType;
+    }
+
+    public MilkType getMilk() {
+        return milk;
+    }
+
+    public List<TeaFlavor> getFlavors() {
+        return Collections.unmodifiableList(flavors);
+    }
+
+    @Override
+    public double getPrice() {
+        double totalPrice = calculateSharedPrice();
+        totalPrice += milk.getPrice();
+
+        if (!flavors.isEmpty()) { //add constant flavor price
+            totalPrice += flavors.size() * flavors.get(0).getPrice();
+        }
+
+        return totalPrice;
+    }
+
+    // *** SETTERS ***
+    public void setTeaType(TeaType teaType) {
+        this.teaType = teaType;
+    }
+
+    public void setMilk(MilkType milk) {
+        this.milk = milk;
+    }
+
+    public void addFlavor(TeaFlavor flavor) {
+        flavors.add(flavor);
+    }
+
+    public void removeFlavor(TeaFlavor flavor) {
+        flavors.remove(flavor);
+    }
 }
