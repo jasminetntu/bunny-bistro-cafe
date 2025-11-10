0f86b438e6b679a499213df037521095bd3a4cd1
Jasmine <jasminetntu@gmail.com>
2025-11-10 14:08:27 -0800

    implement entree enum (#19)

--- src/main/java/com/bunnybistrocafe/models/Entree.java
+++ src/main/java/com/bunnybistrocafe/models/Entree.java
@@ -1,4 +1,44 @@
 package com.bunnybistrocafe.models;
 
 public enum Entree implements MenuItem {
+    EGG_SANDO ("ES", "Egg Sando", 7.00), HOUSE_SALAD ("HS", "House Salad", 10.00),
+    CHICKEN_KATSU_SANDO ("CKS", "Chicken Katsu Sando", 12.50), SAVORY_CREPE ("SC", "Savory Crepe", 15.00);
+
+    private final String abbreviation;
+    private final String name;
+    private final double price;
+
+    Entree(String abbreviation, String name, double price) {
+        this.abbreviation = abbreviation;
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getAbbreviation() {
+        return abbreviation;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public double getPrice() {
+        return price;
+    }
+
+    public Entree fromAbbreviation(String abbrev) {
+        abbrev = abbrev.trim().toUpperCase();
+
+        // iterate through all enum values
+        for (Entree option : Entree.values()) {
+            // check if input letter matches associated letter
+            if (option.abbreviation.equals(abbrev)) {
+                return option;
+            }
+        }
+
+        // if no match is found, return null
+        throw new IllegalArgumentException();
+    }
 }
