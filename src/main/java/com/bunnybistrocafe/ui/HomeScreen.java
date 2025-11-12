6edc45bf1538abcb340cadcb02da6974734d8d44
Jasmine <jasminetntu@gmail.com>
2025-11-12 15:17:05 -0800

    implement HomeScreen class (#23)

--- src/main/java/com/bunnybistrocafe/ui/HomeScreen.java
+++ src/main/java/com/bunnybistrocafe/ui/HomeScreen.java
@@ -1,8 +1,41 @@
 package com.bunnybistrocafe.ui;
 
+import com.bunnybistrocafe.enumerations.HomeOption;
+import com.bunnybistrocafe.util.UserInterface;
+
+import java.util.Scanner;
+
 public class HomeScreen implements Screen {
+    private final Scanner scnr;
+
+    public HomeScreen(Scanner scnr) {
+        this.scnr = scnr;
+    }
+
     @Override
     public void displayScreen() {
+        boolean isRunning = true;
+        HomeOption homeChoice;
 
+        while (isRunning) {
+            UserInterface.printHomeMenu();
+
+            try {
+                homeChoice = HomeOption.fromAbbreviation(scnr.nextLine());
+
+                switch (homeChoice) {
+                    case NEW_ORDER -> {
+                        OrderScreen os = new OrderScreen();
+                        os.displayScreen();
+                    }
+                    case EXIT -> {
+                        isRunning = false;
+                    }
+                }
+            }
+            catch (IllegalArgumentException e) {
+                System.out.println("Invalid input. Must be N or X.");
+            }
+        }
     }
 }
