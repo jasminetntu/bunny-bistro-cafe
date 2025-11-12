e4df795e274e6c00f1d4358c3cc4d426433dd939
Jasmine <jasminetntu@gmail.com>
2025-11-12 15:56:57 -0800

    set up EntreeScreen class (#38)

--- src/main/java/com/bunnybistrocafe/ui/EntreeScreen.java
+++ src/main/java/com/bunnybistrocafe/ui/EntreeScreen.java
@@ -1,8 +1,45 @@
 package com.bunnybistrocafe.ui;
 
+import com.bunnybistrocafe.controllers.OrderManager;
+import com.bunnybistrocafe.enumerations.ActionOption;
+import com.bunnybistrocafe.models.Entree;
+import com.bunnybistrocafe.util.UserInterface;
+
+import java.util.Scanner;
+
 public class EntreeScreen implements Screen {
+    private final Scanner scnr;
+    private final OrderManager orderManager;
+
+    public EntreeScreen(Scanner scnr, OrderManager orderManager) {
+        this.scnr = scnr;
+        this.orderManager = orderManager;
+    }
+
     @Override
     public void displayScreen() {
+        boolean isRunning = true;
+        Entree entreeChoice;
 
+        while (isRunning) {
+            UserInterface.printHomeMenu();
+            String input = scnr.nextLine().trim();
+
+            // go back to previous screen if R
+            if (ActionOption.RETURN.getAbbreviation().equalsIgnoreCase(input)) {
+                isRunning = false;
+            }
+            else { // compare w pastry options
+                try {
+                    entreeChoice = Entree.fromNum(Integer.parseInt(input));
+
+                    //todo: add to order here
+                } catch (IllegalArgumentException e) {
+                    System.out.println("Invalid input. Must be N or X.");
+                }
+            }
+        }
     }
+}
+
 }
