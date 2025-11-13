b2b162b1bcac61225b4a71cbba58c6d48e773e73
Jasmine <jasminetntu@gmail.com>
2025-11-12 19:43:14 -0800

    small basic matcha unit test for DrinkCustomizer (#31)

--- src/test/java/com/bunnybistrocafe/controllers/DrinkCustomizerTest.java
+++ src/test/java/com/bunnybistrocafe/controllers/DrinkCustomizerTest.java
@@ -1,7 +1,10 @@
 package com.bunnybistrocafe.controllers;
 
-import com.bunnybistrocafe.models.Drink;
+import com.bunnybistrocafe.enumerations.drinkcustomizations.*;
+import com.bunnybistrocafe.enumerations.drinkcustomizations.SweetenerType;
+import com.bunnybistrocafe.models.*;
 
+import java.io.ByteArrayInputStream;
 import java.util.Scanner;
 
 import org.junit.jupiter.api.Test;
@@ -11,12 +14,36 @@ class DrinkCustomizerTest {
     @Test
     public void customizeDrink_smallBasicMatcha() {
         // arrange
-        DrinkCustomizer dc = new DrinkCustomizer(new Scanner(System.in));
+        // create simulated user input & feed into scanner
+        String simulatedInput = String.join("\n",
+                "S",      // size
+                "M",      // drink type (Matcha)
+                "I",      // iced
+                "C",      // sweetener type -> cane
+                "100",    // sweetness -> 100%, 1
+                "100",    // ice -> 100%, 1
+                "W",      // milk type -> whole
+                "1",       // topping -> boba
+                "N"       // plushie -> no
+        ) + "\n";
+
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(simulatedInput.getBytes());
+        Scanner scnr = new Scanner(inputStream);
+        DrinkCustomizer dc = new DrinkCustomizer(scnr);
 
         // act
-
+        Drink drink = dc.customizeDrink();
 
         // assert
+        assertEquals(DrinkSize.SMALL, drink.getSize());
+        assertInstanceOf(Matcha.class, drink);
+        assertTrue(drink.isIced());
+        assertEquals(SweetenerType.CANE_SUGAR, drink.getSweetener());
+        assertEquals(1.0, drink.getSweetnessLevel());
+        assertEquals(1.0, drink.getIceLevel());
+        assertEquals(MilkType.WHOLE, ((Matcha) drink).getMilk());
+        assertEquals(1, drink.getToppings().size());
+        assertFalse(drink.hasPlushie());
     }
 
 
