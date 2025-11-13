4bb70419b3a95bc559ae5e9cfc23a2aec13be07c
Jasmine <jasminetntu@gmail.com>
2025-11-12 19:05:12 -0800

    create DrinkCustomizerTest.java (#31)

---
+++ src/test/java/com/bunnybistrocafe/controllers/DrinkCustomizerTest.java
@@ -0,0 +1,28 @@
+package com.bunnybistrocafe.controllers;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class DrinkCustomizerTest {
+    @Test
+    public void customizeDrink_SmallBasicMatcha() {
+        // arrange
+
+
+        // act
+
+
+        // assert
+    }
+
+    @Test
+    public void name() {
+        // arrange
+
+
+        // act
+
+
+        // assert
+    }
+}
