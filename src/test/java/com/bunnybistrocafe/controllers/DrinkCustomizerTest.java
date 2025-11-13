2b81663d36230d820286af110cf170dc97e41cb8
Jasmine <jasminetntu@gmail.com>
2025-11-12 20:06:06 -0800

    add basic hot coffee to DrinkCustomizer unit test (#31)

--- src/test/java/com/bunnybistrocafe/controllers/DrinkCustomizerTest.java
+++ src/test/java/com/bunnybistrocafe/controllers/DrinkCustomizerTest.java
@@ -16,8 +16,8 @@ class DrinkCustomizerTest {
         // arrange
         // create simulated user input & feed into scanner
         String simulatedInput = String.join("\n",
-                "S",      // size
-                "M",      // drink type (Matcha)
+                "S",      // size -> small
+                "M",      // drink type -> matcha
                 "I",      // iced
                 "C",      // sweetener type -> cane
                 "100",    // sweetness -> 100%, 1
@@ -30,6 +30,7 @@ class DrinkCustomizerTest {
         ByteArrayInputStream inputStream = new ByteArrayInputStream(simulatedInput.getBytes());
         Scanner scnr = new Scanner(inputStream);
         DrinkCustomizer dc = new DrinkCustomizer(scnr);
+        double expectedPrice = 6.45;
 
         // act
         Drink drink = dc.customizeDrink();
@@ -44,6 +45,46 @@ class DrinkCustomizerTest {
         assertEquals(MilkType.WHOLE, ((Matcha) drink).getMilk());
         assertEquals(0, drink.getToppings().size());
         assertFalse(drink.hasPlushie());
+        assertEquals(expectedPrice, drink.getPrice());
+    }
+
+    @Test
+    public void customizeDrink_smallBasicHotCoffee() {
+        // arrange
+        // create simulated user input & feed into scanner
+        String simulatedInput = String.join("\n",
+                "S",      // size -> small
+                "C",      // drink type -> coffee
+                "H",      // hot
+                "",       // flavors -> none
+                "C",      // sweetener type -> cane
+                "100",    // sweetness -> 100%, 1
+                "100",    // ice -> 100%, 1
+                "W",      // milk type -> whole
+                "",       // topping -> none
+                "N"       // plushie -> no
+        ) + "\n";
+
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(simulatedInput.getBytes());
+        Scanner scnr = new Scanner(inputStream);
+        DrinkCustomizer dc = new DrinkCustomizer(scnr);
+        double expectedPrice = 5.95;
+
+        // act
+        Drink drink = dc.customizeDrink();
+
+        // assert
+        assertEquals(DrinkSize.SMALL, drink.getSize());
+        assertInstanceOf(Coffee.class, drink);
+        assertFalse(drink.isIced());
+        assertEquals(0, ((Coffee) drink).getFlavors().size());
+        assertEquals(SweetenerType.CANE_SUGAR, drink.getSweetener());
+        assertEquals(1.0, drink.getSweetnessLevel());
+        assertEquals(1.0, drink.getIceLevel());
+        assertEquals(MilkType.WHOLE, ((Coffee) drink).getMilk());
+        assertEquals(0, drink.getToppings().size());
+        assertFalse(drink.hasPlushie());
+        assertEquals(expectedPrice, drink.getPrice());
     }
 
 
